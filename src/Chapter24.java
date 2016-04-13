4789bbd4c901ca7b62d8eb7268ca6253b6d0d1ca
sky <jsky.johnson@gmail.com>
2016-04-13 08:12:49 -0600

    initilizes

---
+++ src/Chapter24.java
@@ -0,0 +1,11 @@
+// Chapter 24 Java Methods
+//Sky Johnson
+// Questions 1,2,3,6,7,8,10,12a,14a, 16, 17, 18, 19
+public class Chapter24 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
